cf8cdf0072c8661d5da3d5dca14be2b985e1e663
Konstantin Lindner <hello@konte.se>
2022-12-09 01:17:28 -0800

    Lots of changes

---
+++ script.js
@@ -0,0 +1,17 @@
+const hamburgerButton = document.querySelector("#hamburger-button");
+
+const closeButton = document.querySelector("#close-button");
+
+hamburgerButton.addEventListener("click", function () {
+  if ((document.getElementById("close-button").style.display = "none")) {
+    document.getElementById("hamburger-button").style.display = "none";
+    document.getElementById("close-button").style.display = "block";
+  }
+});
+
+closeButton.addEventListener("click", function () {
+  if ((document.getElementById("close-button").style.display = "block")) {
+    document.getElementById("hamburger-button").style.display = "block";
+    document.getElementById("close-button").style.display = "none";
+  }
+});
