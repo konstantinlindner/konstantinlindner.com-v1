4744896bb453ea7637247995ef7c958d81833725
Konstantin Lindner <hello@konte.se>
2023-01-02 02:11:33 +0100

    mobile menu rework

--- script.js
+++ script.js
@@ -17,3 +17,53 @@ closeButton.addEventListener("click", function () {
     document.getElementById("mobile-menu").style.display = "none";
   }
 });
+
+const swedishNumber = [
+  "Sweden",
+  "Denmark",
+  "Norway",
+  "Germany",
+  "Netherlands",
+  "Spain",
+  "Switzerland",
+];
+
+const canadianNumber = ["Canada", "United States"];
+
+let phoneLink = document.getElementById("phone-number");
+
+if (!getCookie("phoneNumber")) {
+  fetch("https://ipapi.co/json/")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (swedishNumber.includes(data.country_name)) {
+        setCookie("phoneNumber", "tel:+46 76-007 86 70", 30);
+        phoneLink.href = getCookie("phoneNumber");
+      } else if (canadianNumber.includes(data.country_name)) {
+        setCookie("phoneNumber", "tel:+1 (778) 930-1924", 30);
+        phoneLink.href = getCookie("phoneNumber");
+      } else {
+        setCookie("phoneNumber", "tel:null", 30);
+      }
+    });
+} else {
+  phoneLink.href = getCookie("phoneNumber");
+}
+
+function getCookie(name) {
+  let value = "; " + document.cookie;
+  let parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+    let date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
